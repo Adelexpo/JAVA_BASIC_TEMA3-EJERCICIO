6eb117268f271ce407e65a8a48512b0b371ba36b
AdilDev <adilexpo2@gmail.com>
2022-11-04 13:42:39 +0100

    EjercicioTema3.java_basic

---
+++ src/EjercicioTema3.java
@@ -0,0 +1,10 @@
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+
+        String Ciudad1 = " Barcelona";
+        String Ciudad2 = "Paris";
+        String Ciudad3 = "Roma";
+
+    System.out.println(Ciudad1 + " " + Ciudad2 + " " + Ciudad3);
+    }
+}
